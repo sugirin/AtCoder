d38b3e05d0ffafd5607017fce9c8ac935e0c12f1
Sugirin <sugirin3621@gmail.com>
2021-07-31 20:43:33 +0900

    prepare.shを改良 問題は6問固定じゃないらしい

--- prepare.sh
+++ prepare.sh
@@ -1,9 +1,9 @@
 contest=$1
 
-for q in a b c d e f; do
+for q in a b c d e f g h; do
 cat - <<EOF >src/${contest}_${q}.py
 """
-
+https://atcoder.jp/contests/${contest}/tasks/${contest}_${q}
 """
 EOF
 done
--- src/abc212_c.py
+++ src/abc212_c.py
@@ -1,3 +1,3 @@
 """
-
+https://atcoder.jp/contests/abc212/tasks/abc212_c
 """
