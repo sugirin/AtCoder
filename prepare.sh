1c961d0012f59e86625fee7898ecda1b81668ad3
Sugirin <sugirin3621@gmail.com>
2021-07-31 20:52:00 +0900

    ディレクトリ構成を変更

--- prepare.sh
+++ prepare.sh
@@ -1,7 +1,8 @@
 contest=$1
 
+mkdir -p src/${contest}
 for q in a b c d e f g h; do
-cat - <<EOF >src/${contest}_${q}.py
+cat - <<EOF >src/${contest}/${contest}_${q}.py
 """
 https://atcoder.jp/contests/${contest}/tasks/${contest}_${q}
 """
